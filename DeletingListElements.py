1ef36859dbaeda13e422f2cea2c5099d54d033de
Sagor0sud <mehedihasan74364@gmail.com>
2022-09-18 05:01:54 +0600

    deletingWork

---
+++ DeletingListElements.py
@@ -0,0 +1,13 @@
+Deleting = ['love', 'sex', 'Go', 'romove', 10, 20, 30]
+print(Deleting)
+# Output is
+# ['love', 'sex', 'Go', 'romove', 10, 20, 30]
+del Deleting[1]
+print(Deleting)
+# Output is
+# ['love', 'Go', 'romove', 10, 20, 30]
+
+Deleting.remove('Go')
+print(Deleting)
+# Output is
+# ['love', 'romove', 10, 20, 30]
