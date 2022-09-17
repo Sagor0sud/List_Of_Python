5221d5c7098d58ea42b0a5605c39bb537d59b5b2
Sagor0sud <mehedihasan74364@gmail.com>
2022-09-18 02:59:23 +0600

    typeOF

---
+++ TypesOfList.py
@@ -0,0 +1,48 @@
+#  01. String Data Data Type
+
+
+String = ['Math', 'Physics', 'Chemistry', 'History']
+print(String)
+
+print(String[0])
+print(String[1])
+
+print(String[2])
+
+print(String[3])
+
+
+String1 = ['Munny', 'Sami', 'Sadia', 'Sopna',
+           'Sagor', 'Hasan', 'Ail', "Mohammd"]
+print(String1)
+
+print(String1[0])
+
+print(String1[1])
+print(String1[2])
+
+print(String1[3])
+
+print(String1[4])
+
+print(String1[5])
+
+print(String1[6])
+
+print(String1[7])
+
+
+# 2. Integer Data Type List
+Numbers = [5, 8, 9, 12, 25, 30, 14, 52]
+print(Numbers)
+print(Numbers[2])
+print(Numbers[3])
+print(Numbers[4])
+print(Numbers[5])
+
+# 3. Floating Data Type List
+Numbers1 = [5.4, 6.5, 11.2, 15.9, 21.19]
+print(Numbers1)
+print(Numbers1[4])
+
+# 4. More String Data Type List
